cc61d10615a32631cb7c184711d5e8365e1df447
Luis Nasser <luishnc93@gmail.com>
2020-10-22 00:47:26 -0400

    Adding players movements and reorganized folders

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,14 +3,25 @@
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
+    private float speed = 5f;
+
     void Start()
     {
-        Debug.Log("Hello!");
+        Debug.Log("Start is called");
+        //Initial position
+        transform.position = new Vector3(0,0,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Hello!");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime );
+        transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
     }
+
+
 }
