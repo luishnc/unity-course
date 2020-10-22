c716f79b4aaf6c28803dd77d72cd81a8290b0252
Luis Nasser <luishnc93@gmail.com>
2020-10-22 01:32:06 -0400

    Added control to not let the player cross over screen limit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Xml.Serialization;
+using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
-    private float speed = 5f;
+    [SerializeField] private float speed = 5f;
+
 
     void Start()
     {
@@ -15,12 +17,36 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        movement();
+    }
+
+    private void movement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-
-        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime );
+        //let player move up/down or left/right
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
         transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+
+        //adding control to not cross over the map limit
+        if (transform.position.x > 8.0f)
+        {
+            transform.position = new Vector3(8, transform.position.y, 0);
+        }
+        else if (transform.position.x < -8.0f)
+        {
+            transform.position = new Vector3(-8, transform.position.y, 0);
+        }
+        else if (transform.position.y > 4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, 4.2f, 0);
+        }
+        else if (transform.position.y < -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
     }
 
 
