ede2a98f7754376cfd78b20de9396aaac05a662f
Luis Nasser <luishnc93@gmail.com>
2020-10-21 23:40:37 -0400

    Adding background and player to the main scene

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hello!");
+    }
+}
