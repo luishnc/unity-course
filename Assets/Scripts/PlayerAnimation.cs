36b19cdb87fafd587075af911a55bdb36649a16f
Luis Nasser <luishnc93@gmail.com>
2020-10-24 23:01:51 -0400

    Added animatiosn for when the user press left or right arrow keys

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            anim.SetBool("Turn_Left", true);
+            anim.SetBool("Turn_Right", false);
+        }
+
+         else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            anim.SetBool("Turn_Left", false);
+            anim.SetBool("Turn_Right", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.SetBool("Turn_Left", false);
+            anim.SetBool("Turn_Right", true);
+        } else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            anim.SetBool("Turn_Left", false);
+            anim.SetBool("Turn_Right", false);
+        }
+        
+    }
+}
